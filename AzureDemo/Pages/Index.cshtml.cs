fbda3c9ec35ae5e51885b7abbb6497a966579357
Afzal Pasha <afzal.pasha@hotmail.com>
2023-04-16 11:41:29 +0530

    Connection String Changes

--- AzureDemo/Pages/Index.cshtml.cs
+++ AzureDemo/Pages/Index.cshtml.cs
@@ -1,25 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AzureDemo.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["key1"] = Configuration["key1"];
+            ViewData["key2"] = Configuration["key2"];
+            ViewData["key3"] = Configuration["key3"];
+            ViewData["connectionString"] = Configuration.GetConnectionString("dbConnectionName");
         }
     }
 }
